14e98113ccb54c22680dfcc697e7a7042177e673
sangjun <987654321123sin@gmail.com>
2024-06-13 00:00:26 +0900

    Refactor: 토큰 만료 시간 10->30분으로 변경

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -17,7 +17,7 @@ export const register = async ({ id, password, nickname }) => {
 
 export const login = async ({ id, password }) => {
   try {
-    const response = await axios.post(`${AUTH_API_HOST}/login?expiresIn=10m`, {
+    const response = await axios.post(`${AUTH_API_HOST}/login?expiresIn=30m`, {
       id,
       password,
     });
