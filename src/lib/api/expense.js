fd0ebf3bc3f1629974e6ef9e4882e24e0ea62eb6
sangjun <987654321123sin@gmail.com>
2024-06-13 00:19:01 +0900

    Feat: TanStackQuery 추가 및 반영(CR) 구현

--- src/lib/api/expense.js
+++ src/lib/api/expense.js
@@ -5,9 +5,21 @@ const JSON_SERVER_HOST = "http://localhost:5000";
 export const getExpenses = async () => {
   try {
     const response = await axios.get(`${JSON_SERVER_HOST}/expenses`);
-
     return response.data;
   } catch (error) {
-    alert("expense error!!");
+    alert("getExpenses error!!");
+  }
+};
+
+export const postExpense = async (newExpense) => {
+  try {
+    const { data } = await axios.post(
+      `${JSON_SERVER_HOST}/expenses`,
+      newExpense
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    alert("postExpense error!!");
   }
 };
